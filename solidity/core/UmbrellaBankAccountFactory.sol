ac2c4c2954994b338a3069efcb6ab7cd08247fbf
Tony Kunz <42658482+cryptotwilight@users.noreply.github.com>
2022-12-11 01:57:39 +0000

    Updated

--- solidity/core/UmbrellaBankAccountFactory.sol
+++ solidity/core/UmbrellaBankAccountFactory.sol
@@ -1,68 +1,77 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^ 0.8.17;
+pragma solidity ^0.8.17;
+
+import "https://github.com/umbrellabank/umbrella-bank-crypto-personal/blob/52464a0f08adc8f88ce7260efe2a10f06fa20259/solidity/core/UmbrellaBankAccountFactory.sol";
 
 import "https://github.com/Block-Star-Logic/open-version/blob/e161e8a2133fbeae14c45f1c3985c0a60f9a0e54/blockchain_ethereum/solidity/V1/interfaces/IOpenVersion.sol";
 
-import "../interfaces/IUmbrellaBankAccountFactory.sol";
+import "../interfaces/IUBAFParentFactory.sol";
 
-import "./UmbrellaBankPersonalAccount.sol";
+contract UBAFParentFactory is IUBAFParentFactory, IOpenVersion { 
 
-
-contract UmbrellaBankAccountFactory is IUmbrellaBankAccountFactory, IOpenVersion { 
-
-    string constant name = "RESERVED_UMBRELLA_BANK_CRYPTO_ACCOUNT_FACTORY";
+    string constant name = "RESERVED_UMBRELLA_BANK_ACCOUNT_FACTORY_PARENT_FACTORY";
     uint256 constant version = 1; 
 
-    address owner; 
-    address bank;     
-    address [] accounts; 
+    address admin; 
+    address faucet; 
+    address self; 
 
-    constructor(address _owner) {
-        owner = _owner; 
-    }
+    mapping(address=>bool) registeredBankByAddress; 
+    mapping(address=>bool) factoryIssuedByAddress; 
 
-    function getName() pure external returns (string memory _name) {
-        return name; 
+    address [] afs; 
+
+    constructor (address _admin) {
+        admin = _admin;
+        self = address(self);
     }
 
     function getVersion() pure external returns (uint256 _version) {
         return version; 
     }
 
-    function getBank() view external returns (address _bank) {
-        return bank;
+    function getName() pure external returns (string memory _name) {
+        return name; 
     }
 
-    function getAccounts() view external returns(address [] memory _accounts) {      
-        ownerOrBankOnly() ;
-        return accounts; 
+    function getFaucet() view external returns (address _faucet) {
+        return faucet; 
     }
 
-    function getAccount(address _erc20) external returns (address _account) {
-        require(msg.sender == bank, " bank only ");
-        UmbrellaBankPersonalAccount account_ = new UmbrellaBankPersonalAccount(bank, _erc20);
-        _account = address(account_);
-        accounts.push(_account);
-        return _account; 
+    function getIssuedAccountFactories() view external returns (address [] memory _afs) {
+        adminOnly(); 
+        return afs; 
     }
 
-    function setBank() external returns (bool _set) {
-        require(owner == IUmbrellaBankPersonal(msg.sender).getOwner(), " owner mismatch "); 
-        bank = msg.sender; 
+    function setFaucet(address _faucet) external returns (bool _set) {
+        adminOnly();
+        faucet = _faucet; 
         return true; 
     }
 
-    function changeOwner(address newOwner) external returns (bool _changed){
-        require(msg.sender == owner, "unknown user");
-        owner = newOwner; 
+    function registerBank(address _bank) external returns (bool _registered) {
+        require(msg.sender == faucet, " faucet only " );
+        require(!registeredBankByAddress[_bank], " unregistered banks only ");
+        registeredBankByAddress[_bank] = true; 
         return true; 
     }
 
-    // ======================================= INTERNAL =============================================
+    function createUBAF(address _owner) external returns (address _ubaf) {
+        require(msg.sender == faucet, " faucet only " );
+        UmbrellaBankAcountFactory ubaf_ = new UmbrellaBankAcountFactory(_owner);  
+        factoryIssuedByAddress[msg.sender] = true;       
+        _ubaf = address(ubaf_);
+        afs.push(_ubaf);
+        return _ubaf;
+    }
 
-    function ownerOrBankOnly() view internal returns (bool) {
-        require(msg.sender == owner || msg.sender == bank, " unknown user ");
+    // ========================  INTERNAL ===================
+
+
+    function adminOnly() view internal returns (bool) {
+        require(msg.sender == admin, " admin only ");
         return true; 
     }
+
 
 }
